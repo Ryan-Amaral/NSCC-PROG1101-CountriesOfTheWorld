2378979a7ebef9979f5a8ba9eaff56b7b272da0d
Ryan-Amaral <ryan_amaral@live.com>
2017-04-30 15:52:34 -0300

    initial

---
+++ CountriesOfTheWorld/Form1.Designer.vb
@@ -0,0 +1,209 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormCountriesOfTheWorld
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.LabelChooseCountry = New System.Windows.Forms.Label()
+        Me.ListBoxCountries = New System.Windows.Forms.ListBox()
+        Me.LabelCountryName = New System.Windows.Forms.Label()
+        Me.PictureBoxFlag = New System.Windows.Forms.PictureBox()
+        Me.LabelPopulation = New System.Windows.Forms.Label()
+        Me.TextBoxPopulationValue = New System.Windows.Forms.TextBox()
+        Me.LabelTotalAreaIn = New System.Windows.Forms.Label()
+        Me.ComboBoxMiKM = New System.Windows.Forms.ComboBox()
+        Me.LabelTotalAreaValue = New System.Windows.Forms.Label()
+        Me.LabelPopDensity = New System.Windows.Forms.Label()
+        Me.LabelPopDensityValue = New System.Windows.Forms.Label()
+        Me.LabelPercentWorldPop = New System.Windows.Forms.Label()
+        Me.LabelPercentWorldPopValue = New System.Windows.Forms.Label()
+        Me.ButtonUpdateWorldPop = New System.Windows.Forms.Button()
+        CType(Me.PictureBoxFlag, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'LabelChooseCountry
+        '
+        Me.LabelChooseCountry.AutoSize = True
+        Me.LabelChooseCountry.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelChooseCountry.Location = New System.Drawing.Point(12, 9)
+        Me.LabelChooseCountry.Name = "LabelChooseCountry"
+        Me.LabelChooseCountry.Size = New System.Drawing.Size(180, 13)
+        Me.LabelChooseCountry.TabIndex = 0
+        Me.LabelChooseCountry.Text = "Choose a country from the list:"
+        '
+        'ListBoxCountries
+        '
+        Me.ListBoxCountries.FormattingEnabled = True
+        Me.ListBoxCountries.Location = New System.Drawing.Point(12, 31)
+        Me.ListBoxCountries.Name = "ListBoxCountries"
+        Me.ListBoxCountries.Size = New System.Drawing.Size(177, 368)
+        Me.ListBoxCountries.TabIndex = 1
+        '
+        'LabelCountryName
+        '
+        Me.LabelCountryName.AutoSize = True
+        Me.LabelCountryName.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LabelCountryName.Location = New System.Drawing.Point(198, 19)
+        Me.LabelCountryName.Name = "LabelCountryName"
+        Me.LabelCountryName.Size = New System.Drawing.Size(122, 20)
+        Me.LabelCountryName.TabIndex = 2
+        Me.LabelCountryName.Text = "Country Name"
+        '
+        'PictureBoxFlag
+        '
+        Me.PictureBoxFlag.Location = New System.Drawing.Point(202, 63)
+        Me.PictureBoxFlag.Name = "PictureBoxFlag"
+        Me.PictureBoxFlag.Size = New System.Drawing.Size(246, 161)
+        Me.PictureBoxFlag.TabIndex = 3
+        Me.PictureBoxFlag.TabStop = False
+        '
+        'LabelPopulation
+        '
+        Me.LabelPopulation.AutoSize = True
+        Me.LabelPopulation.Location = New System.Drawing.Point(296, 244)
+        Me.LabelPopulation.Name = "LabelPopulation"
+        Me.LabelPopulation.Size = New System.Drawing.Size(60, 13)
+        Me.LabelPopulation.TabIndex = 4
+        Me.LabelPopulation.Text = "Population:"
+        '
+        'TextBoxPopulationValue
+        '
+        Me.TextBoxPopulationValue.Location = New System.Drawing.Point(362, 241)
+        Me.TextBoxPopulationValue.Name = "TextBoxPopulationValue"
+        Me.TextBoxPopulationValue.Size = New System.Drawing.Size(95, 20)
+        Me.TextBoxPopulationValue.TabIndex = 5
+        Me.TextBoxPopulationValue.Text = "0,000"
+        '
+        'LabelTotalAreaIn
+        '
+        Me.LabelTotalAreaIn.AutoSize = True
+        Me.LabelTotalAreaIn.Location = New System.Drawing.Point(219, 272)
+        Me.LabelTotalAreaIn.Name = "LabelTotalAreaIn"
+        Me.LabelTotalAreaIn.Size = New System.Drawing.Size(67, 13)
+        Me.LabelTotalAreaIn.TabIndex = 6
+        Me.LabelTotalAreaIn.Text = "Total Area in"
+        '
+        'ComboBoxMiKM
+        '
+        Me.ComboBoxMiKM.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBoxMiKM.FormattingEnabled = True
+        Me.ComboBoxMiKM.Items.AddRange(New Object() {"Sq. Mi.", "Sq. KM"})
+        Me.ComboBoxMiKM.Location = New System.Drawing.Point(292, 269)
+        Me.ComboBoxMiKM.Name = "ComboBoxMiKM"
+        Me.ComboBoxMiKM.Size = New System.Drawing.Size(64, 21)
+        Me.ComboBoxMiKM.TabIndex = 7
+        '
+        'LabelTotalAreaValue
+        '
+        Me.LabelTotalAreaValue.AutoSize = True
+        Me.LabelTotalAreaValue.Location = New System.Drawing.Point(362, 272)
+        Me.LabelTotalAreaValue.Name = "LabelTotalAreaValue"
+        Me.LabelTotalAreaValue.Size = New System.Drawing.Size(49, 13)
+        Me.LabelTotalAreaValue.TabIndex = 8
+        Me.LabelTotalAreaValue.Text = "0,000.00"
+        '
+        'LabelPopDensity
+        '
+        Me.LabelPopDensity.AutoSize = True
+        Me.LabelPopDensity.Location = New System.Drawing.Point(204, 302)
+        Me.LabelPopDensity.Name = "LabelPopDensity"
+        Me.LabelPopDensity.Size = New System.Drawing.Size(152, 13)
+        Me.LabelPopDensity.TabIndex = 9
+        Me.LabelPopDensity.Text = "Population Density per Sq. Mi.:"
+        '
+        'LabelPopDensityValue
+        '
+        Me.LabelPopDensityValue.AutoSize = True
+        Me.LabelPopDensityValue.Location = New System.Drawing.Point(362, 302)
+        Me.LabelPopDensityValue.Name = "LabelPopDensityValue"
+        Me.LabelPopDensityValue.Size = New System.Drawing.Size(49, 13)
+        Me.LabelPopDensityValue.TabIndex = 10
+        Me.LabelPopDensityValue.Text = "0,000.00"
+        '
+        'LabelPercentWorldPop
+        '
+        Me.LabelPercentWorldPop.AutoSize = True
+        Me.LabelPercentWorldPop.Location = New System.Drawing.Point(195, 332)
+        Me.LabelPercentWorldPop.Name = "LabelPercentWorldPop"
+        Me.LabelPercentWorldPop.Size = New System.Drawing.Size(161, 13)
+        Me.LabelPercentWorldPop.TabIndex = 11
+        Me.LabelPercentWorldPop.Text = "Percentage of World Population:"
+        '
+        'LabelPercentWorldPopValue
+        '
+        Me.LabelPercentWorldPopValue.AutoSize = True
+        Me.LabelPercentWorldPopValue.Location = New System.Drawing.Point(362, 332)
+        Me.LabelPercentWorldPopValue.Name = "LabelPercentWorldPopValue"
+        Me.LabelPercentWorldPopValue.Size = New System.Drawing.Size(45, 13)
+        Me.LabelPercentWorldPopValue.TabIndex = 12
+        Me.LabelPercentWorldPopValue.Text = "00.00 %"
+        '
+        'ButtonUpdateWorldPop
+        '
+        Me.ButtonUpdateWorldPop.Location = New System.Drawing.Point(350, 367)
+        Me.ButtonUpdateWorldPop.Name = "ButtonUpdateWorldPop"
+        Me.ButtonUpdateWorldPop.Size = New System.Drawing.Size(107, 23)
+        Me.ButtonUpdateWorldPop.TabIndex = 13
+        Me.ButtonUpdateWorldPop.Text = "Update Population"
+        Me.ButtonUpdateWorldPop.UseVisualStyleBackColor = True
+        '
+        'FormCountriesOfTheWorld
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(469, 411)
+        Me.Controls.Add(Me.ButtonUpdateWorldPop)
+        Me.Controls.Add(Me.LabelPercentWorldPopValue)
+        Me.Controls.Add(Me.LabelPercentWorldPop)
+        Me.Controls.Add(Me.LabelPopDensityValue)
+        Me.Controls.Add(Me.LabelPopDensity)
+        Me.Controls.Add(Me.LabelTotalAreaValue)
+        Me.Controls.Add(Me.ComboBoxMiKM)
+        Me.Controls.Add(Me.LabelTotalAreaIn)
+        Me.Controls.Add(Me.TextBoxPopulationValue)
+        Me.Controls.Add(Me.LabelPopulation)
+        Me.Controls.Add(Me.PictureBoxFlag)
+        Me.Controls.Add(Me.LabelCountryName)
+        Me.Controls.Add(Me.ListBoxCountries)
+        Me.Controls.Add(Me.LabelChooseCountry)
+        Me.Name = "FormCountriesOfTheWorld"
+        Me.Text = "Countries of the World"
+        CType(Me.PictureBoxFlag, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents LabelChooseCountry As System.Windows.Forms.Label
+    Friend WithEvents ListBoxCountries As System.Windows.Forms.ListBox
+    Friend WithEvents LabelCountryName As System.Windows.Forms.Label
+    Friend WithEvents PictureBoxFlag As System.Windows.Forms.PictureBox
+    Friend WithEvents LabelPopulation As System.Windows.Forms.Label
+    Friend WithEvents TextBoxPopulationValue As System.Windows.Forms.TextBox
+    Friend WithEvents LabelTotalAreaIn As System.Windows.Forms.Label
+    Friend WithEvents ComboBoxMiKM As System.Windows.Forms.ComboBox
+    Friend WithEvents LabelTotalAreaValue As System.Windows.Forms.Label
+    Friend WithEvents LabelPopDensity As System.Windows.Forms.Label
+    Friend WithEvents LabelPopDensityValue As System.Windows.Forms.Label
+    Friend WithEvents LabelPercentWorldPop As System.Windows.Forms.Label
+    Friend WithEvents LabelPercentWorldPopValue As System.Windows.Forms.Label
+    Friend WithEvents ButtonUpdateWorldPop As System.Windows.Forms.Button
+
+End Class
---
+++ CountriesOfTheWorld/Form1.vb
@@ -0,0 +1,316 @@
+﻿Option Strict On
+
+Imports System.IO
+
+Public Class FormCountriesOfTheWorld
+
+    'the stats for the countries
+    Dim countryNames() As String
+    Dim countryPopulations() As Long
+    Dim countrySizesMiles() As Double
+
+    'keeps track of the current selected country
+    Dim currentCountryId As Integer
+
+    'display country info after first click
+    Dim firstClick As Boolean = True
+
+    Const COUNTRIES_TXT As String = "countries.txt"
+    Const FLAGS_PATH As String = "flags\"
+
+    'multiply miles by an amount to get km
+    Const KM_MULTIPLIER As Double = 1.609344
+
+    Const MILES As String = "Sq. Mi."
+    Const KILOMETERS As String = "Sq. KM"
+
+    Const POP_DENSITY_DEFAULT_STRING As String = "Population Density per "
+
+    'store area of selected country for access from multiple subs
+    Dim currentArea As Double
+
+    ''' <summary>
+    ''' When the form loads, hide some of the controls and displays the countries
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub FormCountriesOfTheWorld_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        'hide the controls
+        ControlsVisibilty(False)
+
+        'load the stats into arrays
+        LoadCountriesFromFile(COUNTRIES_TXT)
+
+        'load the country names into the listbox
+        LoadCountriesListBox()
+
+    End Sub
+
+    ''' <summary>
+    ''' Hides or shows the controls
+    ''' </summary>
+    ''' <param name="isEnabled"></param>
+    ''' <remarks></remarks>
+    Private Sub ControlsVisibilty(isEnabled As Boolean)
+
+        LabelCountryName.Visible = isEnabled
+        PictureBoxFlag.Visible = isEnabled
+        LabelPopulation.Visible = isEnabled
+        TextBoxPopulationValue.Visible = isEnabled
+        LabelTotalAreaIn.Visible = isEnabled
+        ComboBoxMiKM.Visible = isEnabled
+        LabelTotalAreaValue.Visible = isEnabled
+        LabelPopDensity.Visible = isEnabled
+        LabelPopDensityValue.Visible = isEnabled
+        LabelPercentWorldPop.Visible = isEnabled
+        LabelPercentWorldPopValue.Visible = isEnabled
+        ButtonUpdateWorldPop.Visible = isEnabled
+
+    End Sub
+
+    ''' <summary>
+    ''' Load the stats for the countries from a text file into arrays
+    ''' </summary>
+    ''' <param name="dataFile"></param>
+    ''' <remarks></remarks>
+    Private Sub LoadCountriesFromFile(dataFile As String)
+
+        'open the text file
+        Dim reader As StreamReader = File.OpenText(dataFile)
+
+        'a string to temporarily hold a line of text
+        Dim tempString() As String
+
+        Dim index As Integer = 0
+
+        'read through each line
+        While reader.EndOfStream = False
+
+            'store the values of the line
+            tempString = reader.ReadLine().Split(","c)
+
+            'update the country names
+            ReDim Preserve countryNames(index)
+            countryNames(index) = tempString(0)
+
+            'update the country populations
+            ReDim Preserve countryPopulations(index)
+            countryPopulations(index) = Convert.ToInt64(tempString(1))
+
+            'update the country sizes in miles
+            ReDim Preserve countrySizesMiles(index)
+            countrySizesMiles(index) = Convert.ToDouble(tempString(2))
+
+            index += 1
+
+        End While
+
+        reader.Close()
+
+    End Sub
+
+    ''' <summary>
+    ''' Save any changes in population to the text file
+    ''' </summary>
+    ''' <remarks></remarks>
+    Private Sub SaveCountriesToFile()
+
+        'create the new text file
+        Dim writer As StreamWriter = File.CreateText(COUNTRIES_TXT)
+
+        'write data from arrays into the file
+        For line = 0 To countryNames.Length - 1
+            writer.WriteLine(countryNames(line) & "," & _
+                             countryPopulations(line) & "," & _
+                             countrySizesMiles(line))
+        Next
+
+        writer.Close()
+
+    End Sub
+
+    ''' <summary>
+    ''' Load the country names into the listbox
+    ''' </summary>
+    ''' <remarks></remarks>
+    Private Sub LoadCountriesListBox()
+
+        ListBoxCountries.Items.AddRange(countryNames)
+
+    End Sub
+
+    ''' <summary>
+    ''' When a new country is selected display the stats for that country
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub ListBoxCountries_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBoxCountries.SelectedIndexChanged
+
+        'show the controls for the countries if it is the first country clicked
+        If firstClick = True Then
+            firstClick = False
+            ControlsVisibilty(True)
+            'show default option on the combo box
+            ComboBoxMiKM.Text = MILES
+        End If
+
+        'set the current country selected index
+        currentCountryId = ListBoxCountries.SelectedIndex
+
+        DisplayCountryData()
+
+    End Sub
+
+    ''' <summary>
+    ''' Displays the data of the selected country
+    ''' </summary>
+    ''' <remarks></remarks>
+    Private Sub DisplayCountryData()
+
+        'show flag
+        DisplayCountryFlag()
+
+        'show area
+        DisplayAreaData()
+
+        'show all population stats
+        DisplayPopulationData()
+
+    End Sub
+
+    ''' <summary>
+    ''' Shows the flag of the current country
+    ''' </summary>
+    ''' <remarks></remarks>
+    Sub DisplayCountryFlag()
+
+        'convert spaces to underscores to get appropriate file name
+        Dim flagFileName = countryNames(currentCountryId).Replace(" "c, "_"c)
+        flagFileName &= ".png"
+        PictureBoxFlag.Image = Image.FromFile(FLAGS_PATH & flagFileName)
+
+    End Sub
+
+    ''' <summary>
+    ''' Displays the area of the country and stores its area in a global variable
+    ''' </summary>
+    ''' <remarks></remarks>
+    Sub DisplayAreaData()
+
+        'get the current country's area
+        If ComboBoxMiKM.SelectedItem.ToString() = KILOMETERS Then
+            currentArea = CalculateAreaInKM(countrySizesMiles(currentCountryId))
+        Else
+            currentArea = countrySizesMiles(currentCountryId)
+        End If
+
+        'display the area
+        LabelTotalAreaValue.Text = currentArea.ToString("N2")
+
+    End Sub
+
+    ''' <summary>
+    ''' Displays all info about the population of a country
+    ''' </summary>
+    ''' <remarks></remarks>
+    Private Sub DisplayPopulationData()
+
+        'the population of the country
+        TextBoxPopulationValue.Text = countryPopulations(currentCountryId).ToString("N0")
+
+        'the population density of the country
+        LabelPopDensityValue.Text = (countryPopulations(currentCountryId) / currentArea).ToString("N2")
+
+        'the percent of world population of this country
+        LabelPercentWorldPopValue.Text = ((countryPopulations(currentCountryId) / CalculateTotalWorldPopulation()) * 100).ToString("N2") & " %"
+
+    End Sub
+
+    ''' <summary>
+    ''' Calculates and returns the total world population
+    ''' </summary>
+    ''' <returns></returns>
+    ''' <remarks></remarks>
+    Private Function CalculateTotalWorldPopulation() As Long
+
+        Dim total As Long = 0
+
+        'add each country's population to the total
+        For index = 0 To countryPopulations.Length - 1
+            total += countryPopulations(index)
+        Next
+
+        Return total
+
+    End Function
+
+    ''' <summary>
+    ''' convert square miles into square km
+    ''' </summary>
+    ''' <param name="countryAreaInMiles"></param>
+    ''' <returns></returns>
+    ''' <remarks></remarks>
+    Private Function CalculateAreaInKM(countryAreaInMiles As Double) As Double
+
+        'the square root of the miles multiplied by the km multiplier is square km
+        Dim kmArea As Double = Math.Pow(Math.Sqrt(countryAreaInMiles) * KM_MULTIPLIER, 2)
+        Return kmArea
+
+    End Function
+
+    ''' <summary>
+    ''' Change between metric and imperial when index is changed
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub ComboBoxMiKM_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBoxMiKM.SelectedIndexChanged
+
+        'update the display string
+        If ComboBoxMiKM.SelectedItem.ToString() = MILES Then
+            LabelPopDensity.Text = POP_DENSITY_DEFAULT_STRING & MILES & ":"
+        Else
+            LabelPopDensity.Text = POP_DENSITY_DEFAULT_STRING & KILOMETERS & ":"
+        End If
+
+        're-display the area and population data
+        DisplayAreaData()
+        DisplayPopulationData()
+
+    End Sub
+
+    ''' <summary>
+    ''' Update the population stats if this button is clicked
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub ButtonUpdateWorldPop_Click(sender As Object, e As EventArgs) Handles ButtonUpdateWorldPop.Click
+
+        'remove commas from the string
+        Dim populationText As String = TextBoxPopulationValue.Text.Replace(",", "")
+
+        'check if string is numeric or not
+        If IsNumeric(populationText) Then
+
+            If populationText.Length > 15 Then
+                MessageBox.Show("Input is too large")
+            ElseIf populationText.Contains("."c) Or Convert.ToInt64(populationText) < 0 Then 'numeric but incorrect format
+                MessageBox.Show("Please enter a valid whole number that is greater than or equal to 0")
+            Else 'correct format, save and display changes
+                countryPopulations(currentCountryId) = Convert.ToInt64(populationText)
+                DisplayPopulationData()
+                SaveCountriesToFile()
+                MessageBox.Show("Saved changes to file")
+            End If
+
+        Else 'input is not numeric
+            MessageBox.Show("Please enter a valid whole number")
+        End If
+
+    End Sub
+
+End Class
